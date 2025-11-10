68138409216a71b64d6600821913bf815bd94a81
Maximo Jimenez <142178516+MaxJLCRG@users.noreply.github.com>
2025-11-09 22:40:50 -0300

    LobbyDeModosLocales
    Una representación sin funcion de como serían visualmente las lobbys de los 3 modos locales (clasico, math y time)

--- LudoTime/FrontEnd/src/app/home/play/local/page.js
+++ LudoTime/FrontEnd/src/app/home/play/local/page.js
@@ -12,46 +12,41 @@ export default function LocalPage() {
         { text: "Perfil", href: "../../../navhambar/profile" },
         { text: "Ayuda", href: "../../../navhambar/help" },
         { text: "Configuración", href: "../../../navhambar/settings" },
-        { text: "Cerrar sesión", href:"../../../navhambar/log out" },
+        { text: "Cerrar sesión", href: "../../../navhambar/log out" },
     ];
-
-    /* 
-    const irALocal = () => {
-        router.push("/home/play/local");
-    };
-
-    const irAOnline = () => {
-        router.push("/home/play/online");
-    };
-
-    const irATutorial = () => {
-        router.push("/home/play/tutorial");
-    };
-    */
 
     return (
         <body className={styles.body}>
             <HamburgerMenu items={menuItems} />
             <main>
                 <div className={styles.container}>
-                    <button className={`${styles.card} ${styles.classicbtn}`} /*onClick={}*/>
-                        <div className={styles.overlay}></div> {/* NO BORRAR SIRVE PARA DIFUMINAR EL BOTON*/}
+                    <button
+                        className={`${styles.card} ${styles.classicbtn}`}
+                        onClick={() => router.push("/home/play/local/classic")}
+                    >
+                        <div className={styles.overlay}></div>
                         <div className={styles.content}>
                             <span className={styles.title}>CLASSIC</span>
-                            <span className={styles.desc}>El Ludo de toda la vida. Para los más clasicos y puristas!</span>
+                            <span className={styles.desc}>El Ludo de toda la vida. Para los más clásicos y puristas!</span>
                         </div>
                     </button>
 
-                    <button className={`${styles.card} ${styles.mathbtn}`} /*onClick={}*/>
-                        <div className={styles.overlay}></div> {/* NO BORRAR SIRVE PARA DIFUMINAR EL BOTON*/}
+                    <button
+                        className={`${styles.card} ${styles.mathbtn}`}
+                        onClick={() => router.push("/home/play/local/math")}
+                    >
+                        <div className={styles.overlay}></div>
                         <div className={styles.content}>
                             <span className={styles.title}>MATH</span>
-                            <span className={styles.desc}>Dedicado a matematicos puros. Aquí gana más la mente que la suerte!</span>
+                            <span className={styles.desc}>Dedicado a matemáticos puros. ¡Acá gana más la mente que la suerte!</span>
                         </div>
                     </button>
 
-                    <button className={`${styles.card} ${styles.timebtn}`} /*onClick={}*/>
-                        <div className={styles.overlay}></div> {/* NO BORRAR SIRVE PARA DIFUMINAR EL BOTON*/}
+                    <button
+                        className={`${styles.card} ${styles.timebtn}`}
+                        onClick={() => router.push("/home/play/local/time")}
+                    >
+                        <div className={styles.overlay}></div>
                         <div className={styles.content}>
                             <span className={styles.title}>TIME</span>
                             <span className={styles.desc}>El modo predeterminado de LUDOTIME!</span>
